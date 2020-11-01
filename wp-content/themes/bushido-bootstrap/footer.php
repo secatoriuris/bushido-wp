3d4222489af9365c76f070a13e170620d84d6dcd
secatoriuris <chriedlberger@gmail.com>
2020-11-01 22:49:47 +0100

    some styling.

--- wp-content/themes/bushido-bootstrap/footer.php
+++ wp-content/themes/bushido-bootstrap/footer.php
@@ -18,20 +18,33 @@
 <footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
 	<div class="container pt-3 pb-3">
 		<div class="row">
-			<div class="col-sm-8 col-md-9 footer-col site-info">
-				<div>
-					&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?>
-				</div>
-				<div>
-					<a href="/impressum">Impressum</a>
-					<span class="sep"> | </span>
-					<a href="/datenschutz">Datenschutz</a>
+			<div class="col-sm-4 footer-col">
+				<div class="site-info">
+					<div>
+						&copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
+					</div>
+					<div>
+						<a href="/impressum">Impressum</a>
+						<span class="sep"> | </span>
+						<a href="/datenschutz">Datenschutz</a>
+					</div>
 				</div>
 			</div>
-			<div class="col-sm-2 footer-col site-logo">
-				<a href="<?php echo esc_url( home_url( '/' )); ?>">
-					<img src="<?php echo esc_url(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
-				</a>
+			<div class="col-sm-4 footer-col">
+				<div class="site-logo">
+					<a href="<?php echo esc_url( home_url( '/' )); ?>">
+						<img src="<?php echo esc_url(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+					</a>
+				</div>
+			</div>
+			<div class="col-sm-4 footer-col">
+				<div class="site-social">
+					<div class="social-icon">
+						<a href="https://www.facebook.com/pages/Sportschule-Bushido/178230115548262" target="_blank">
+							<?php echo '<img src="'.home_url().'/wp-content/themes/bushido-bootstrap/logos/facebook.svg">'?>
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- wp-content/themes/bushido-bootstrap/js/blockmod.js
+++ wp-content/themes/bushido-bootstrap/js/blockmod.js
@@ -1,15 +1,20 @@
 wp.blocks.registerBlockStyle( 'core/group', {
-    name: 'center-group',
-    label: 'Zentriert',
-    isDefault: true
+	name: 'center-group',
+	label: 'Zentriert',
+	isDefault: true
 } );
 
 wp.blocks.registerBlockStyle( 'core/group', {
-    name: 'left-group',
-    label: 'Links',
+	name: 'left-group',
+	label: 'Links',
 } );
 
 wp.blocks.registerBlockStyle( 'core/group', {
-    name: 'right-group',
-    label: 'Rechts',
+	name: 'right-group',
+	label: 'Rechts',
+} );
+
+wp.blocks.registerBlockStyle( 'core/button', {
+	name: 'no-frame',
+	label: 'Kein Rahmen',
 } );
